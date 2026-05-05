4845ec088f073198a28cd4edcb4d233cc58f7a66
Gajji Mahesh <gajjimahesh1092@gmail.com>
2026-05-05 14:32:14 +0530

    Add log statement in uploadToCloudinary function

--- backend/config/cloudinaryUpload.js
+++ backend/config/cloudinaryUpload.js
@@ -1,7 +1,7 @@
 import cloudinary from "./cloudinary.js";
 
 export const uploadToCloudinary = (buffer) => {
-  console.log("Upload function called"); // 👈 ADD
+  console.log("Upload function called");
   console.log(buffer);
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream({ folder: "blog_users_b2" }, (err, result) => {
