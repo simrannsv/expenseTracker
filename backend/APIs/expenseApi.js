df31d427d37a73614d343babafdd562664792067
Simran Varthavani <simssV08@gmail.com>
2026-05-05 14:57:36 +0530

    Implement user authentication in expense API
    Added user authentication for expense routes and improved error handling.

--- backend/APIs/expenseApi.js
+++ backend/APIs/expenseApi.js
@@ -1,41 +1,57 @@
-import exp, { response } from'express'
-import { expenseModel } from "../Models/expenseModel.js";
- export const expenseApp=exp.Router()
+import exp from 'express'
+import { expenseModel } from "../Models/expenseModel.js"
+import { verifyToken } from '../Middlewares/verifyToken.js'
 
- let expenses=[]
- //this is to add expense
- expenseApp.post('/addExpense', async (req, res) => {
-  const newExp = new expenseModel(req.body)
-  expenses.push(newExp)
-  await newExp.save()
-  res.json({ message: "Expense Added", payload: newExp })
-})
+export const expenseApp = exp.Router()
 
-//this to see the history of expenses
-expenseApp.get('/readexpenses', (req, res) => {
-    res.json({ message: "All products", payload: expenses }) 
-})
-
-//this to update expenses
-expenseApp.put('/updateExpenses/:id', async (req, res) => {
-  const updatedExpense = await expenseModel.findByIdAndUpdate(
-    req.params.id,
-    { $set: req.body },
-    { new: true }
-  )
-  if (!updatedExpense) {
-    return res.json({ message: "Expense not found" })
+expenseApp.post('/addExpense', verifyToken, async (req, res) => {
+  try {
+    const newExp = new expenseModel({
+      ...req.body,
+      userId: req.user.id  // from token
+    })
+    await newExp.save()
+    res.json({ message: "Expense Added", payload: newExp })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
   }
-  return res.json({ message: "Expense updated", payload: updatedExpense })
 })
 
-expenseApp.delete('/deleteExpenses/:id',async(req,res)=>
-{
-    const deletedExpense=await expenseModel.findByIdAndDelete(req.params.id)
-    if(!deletedExpense)
-    {
-        return res.json({message:"Expense not found"})
+expenseApp.get('/readExpenses', verifyToken, async (req, res) => {
+  try {
+    const expenses = await expenseModel.find({ userId: req.user.id })
+    res.json({ message: "All Expenses", payload: expenses })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
 
+expenseApp.put('/updateExpense/:id', verifyToken, async (req, res) => {
+  try {
+    const updatedExpense = await expenseModel.findByIdAndUpdate(
+    { _id: req.params.id, userId: req.user.id },
+      { $set: req.body },
+      { returnDocument: 'after' }
+    )
+    if (!updatedExpense) {
+      return res.json({ message: "Expense not found" })
+    }
+    return res.json({ message: "Expense updated", payload: updatedExpense })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+expenseApp.delete('/deleteExpense/:id', verifyToken, async (req, res) => {
+  try {
+    const deletedExpense = await expenseModel.findByIdAndDelete(
+       { _id: req.params.id, userId: req.user.id }
+    )
+    if (!deletedExpense) {
+      return res.json({ message: "Expense not found" })
     }
     return res.json({ message: "Expense deleted", payload: deletedExpense })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
